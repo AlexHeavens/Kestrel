968c209745c3210bcce5b0e6eb4ec45982dbae3d
Alexander Heavens <alexander.heavens@gmail.com>
2022-12-31 02:01:34 +0000

    Correct site index

--- src/obscura.py
+++ src/obscura.py
@@ -9,7 +9,7 @@ def main():
     args = parse_args()
     ward_data = get_all_data_by_ward()
     map_obj = generate_interactive_map(ward_data)
-    map_obj.save(f"{args.build_dir}/obscura.html")
+    map_obj.save(f"{args.build_dir}/index.html")
 
 
 def parse_args():
--- src/paths.py
+++ src/paths.py
@@ -2,4 +2,4 @@ import sys
 
 project_dir = f"{sys.path[0]}/.."
 data_dir = f"{project_dir}/data"
-default_build_dir = f"{project_dir}/build"
+default_build_dir = f"{project_dir}"
